9319fb2ba1e0c787387357e44f7f8e45f86bc30b
muskan5guptaaa <muskan5gupta123@gmail.com>
2024-06-01 13:05:06 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const container=document.querySelector(".container");
+const container2=document.querySelector(".container2");
+const rating=document.querySelectorAll(".btn-num");
+const number=document.querySelector(".num");
+const submitBtn=document.querySelector(".submit-btn");
+
+submitBtn.addEventListener("click",function(){
+
+    container.classList.add("hidden");
+    container2.classList.remove("hidden");
+});
+
+
+for(let i=0;i<rating.length;i++){
+    rating[i].addEventListener("click",function(){
+        number.textContent=rating[i].innerHTML;
+    })
+}
